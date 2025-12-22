9dc4c55e9a0cc92b1617d6f878d117207b33a2b9
Swara <swaranatekar546@gmail.com>
2025-12-22 11:18:12 +0530

    feat: add login validation and credential check logic

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { useState } from "react";
 import "./login.css";
+import google from "../assets/icons/google.png";
+import facebook from "../assets/icons/facebook.png";
+import instagram from "../assets/icons/instagram.png";
+import x from "../assets/icons/x.png";
+import signUp from "../assets/icons/signUp.png"
 
 function Login() {
   const navigate = useNavigate();
@@ -17,15 +22,31 @@ function Login() {
       alert("Enter valid email");
       return;
     }
-
     // later backend auth
     navigate("/");
+    const savedUser = localStorage.getItem("user");
+
+  if (!savedUser) {
+    alert("No user registered");
+    return;
+  }
+
+  const { email: savedEmail, password: savedPassword } =
+    JSON.parse(savedUser);
+
+  if (email !== savedEmail || password !== savedPassword) {
+    alert("Email or password is incorrect");
+    return;
+  }
+  navigate("/");
   };
 
   return (
     <div className="page">
       <div className="card">
-        <div className="logo">Logo</div>
+        <div className="mainlogo">
+          <img src={signUp} alt="mainlogo" />
+        </div>
 
         <h2>Welcome Back!</h2>
         <p className="subtitle">Log in to access your crop insights.</p>
@@ -42,11 +63,22 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <p className="forgot">Forgot Password?</p>
+              <p
+        className="forgot"
+        onClick={() => navigate("/forgetpass")}>
+        Forgot Password?
+      </p>
 
         <button className="button" onClick={handleLogin}>
           Login
         </button>
+
+      <div className="logos">
+        <a href="http://www.google.com"><img src={google} alt="google logo"/></a>
+        <a href="http://www.facebook.com"><img src={facebook} alt="facebook logo"/></a>
+        <a href="http://www.insatgram.com"><img src={instagram} alt="instagram logo"/></a>
+        <a href="http://www.twitter.com"><img src={x} alt="x logo"/></a>
+      </div>
 
         <p className="text">
           Don't have an account?{" "}
